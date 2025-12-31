6d9c19f619cf0df54346e0d715d03dc0aa84edfd
Camilo Carmona Ramirez <125711240+Dev19Carmona@users.noreply.github.com>
2025-12-31 12:57:34 -0500

    Update package version to 0.0.22 in package.json and export additional interfaces from the form component module for improved functionality.

--- projects/lib-components/src/lib/components/origin/form/index.ts
+++ projects/lib-components/src/lib/components/origin/form/index.ts
@@ -1,4 +1,5 @@
 export * from './form.component'
 export * from './components/inline-input/inline-input.component'
+export * from './components/inline-input/interfaces/index'
 export * from './interfaces/index'
 export * from './enums/index'
